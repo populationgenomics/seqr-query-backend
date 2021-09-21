44fce6fed34fdc2fcc09214cab8554e2ee3c366f
Michael Franklin <michael.franklin@populationgenomics.org.au>
2021-09-21 20:11:09 +1000

    Better es_to_protobuff implementation

--- translator/es_to_protobuff.py
+++ translator/es_to_protobuff.py
@@ -1,14 +1,20 @@
 from typing import Dict, Optional, List
 
+import json
 from enum import Enum
 from abc import abstractmethod, ABC
 from unittest import TestCase
-from types import SimpleNamespace
 from textwrap import indent
 
 
 MAX_ROWS = 10000
 INDENT = "  "
+
+ARROW_PARTITIONS = [
+    "file:///data/part-00000-2d5ce851-c379-4eab-94ba-1e51f996109b-c000.zstd.arrow",
+    "file:///data/part-00001-2d5ce851-c379-4eab-94ba-1e51f996109b-c000.zstd.arrow",
+    "file:///data/part-00002-2d5ce851-c379-4eab-94ba-1e51f996109b-c000.zstd.arrow",
+]
 
 
 class ExpressionType(Enum):
@@ -24,6 +30,26 @@ class Expression(ABC):
     @abstractmethod
     def to_protobuff(self) -> str:
         pass
+
+    def to_output_query(self, columns: List[str], arrow_urls: List[str]) -> str:
+
+        headers = []
+        # arrow links
+        headers.extend(f'arrow_urls: "{url}"' for url in arrow_urls)
+
+        # fields to select
+        headers.extend(f'projection_columns: "{col}"' for col in columns)
+        str_headers = "\n".join(headers)
+
+        return f"""\
+{str_headers}
+
+filter_expression {{
+{indent(self.to_protobuff(), INDENT)}
+}}
+
+max_rows: {MAX_ROWS}
+"""
 
 
 class Column(Expression):
@@ -58,24 +84,37 @@ class Literal(Expression):
             return "bool_value"
 
     def to_protobuff(self) -> str:
+        literal_value = self.literal
+        if self.type == "string_value":
+            escaped = literal_value.replace('"', '\\"')
+            literal_value = f'"{escaped}"'
+
         return f"""\
 literal {{
-{INDENT}{self.type}: {self.literal}
+{INDENT}{self.type}: {literal_value}
 }}"""
 
 
 class Call(Expression):
     def __init__(
-        self, function_name: str, arguments: List[Expression], options: List[str] = None
+        self,
+        function_name: str,
+        arguments: List[Expression],
+        options: Dict[str, Dict[str, str]] = None,
     ):
+        if not isinstance(arguments, list):
+            raise ValueError(
+                f"Expected arguments to be a list, received: {type(arguments)}"
+            )
+
+        if options is not None and not isinstance(options, dict):
+            raise ValueError(f"Expected options to be dict, received {type(options)}")
+
         self.function_name = function_name
         self.arguments = arguments
-        self.options = options or []
+        self.options = options or {}
 
     def to_protobuff(self) -> str:
-
-        if self.options:
-            raise NotImplementedError(self.to_protobuff)
 
         _str_arguments = "\n".join(
             f"""\
@@ -85,46 +124,32 @@ arguments {{
             for a in self.arguments
         )
 
-        return f"""\
-call {{
-{INDENT}function_name: "{self.function_name}"
+        internals = [f'function_name: "{self.function_name}"', _str_arguments]
 
-{indent(_str_arguments, INDENT)}
-}}"""
+        if self.options:
+            for k, d in self.options.items():
+                inner = "\n".join(f'{kk}: "{vv}"' for kk, vv in d.items())
+                internals.append(f"{k} {{\n{indent(inner, INDENT)}\n}}")
+
+        str_internals = indent("\n".join(internals), INDENT)
+        return f"call {{\n{str_internals}\n}}"
 
 
-def main_input(es: Dict):
-    est = SimpleNamespace(**es)
+def main_input(es: Dict) -> str:
 
-    sort = est.sort
-    skip = getattr(est, "from")
-    size = est.size
-    fields = est._source
+    fields = es.get("_source")
+    fields = ["xpos", "variantId"]
 
-    _str_arrow_urls = ""
-    _str_columns = "\n".join(f'projection_columns: "{col}"' for col in fields)
+    expr = unwrap_query(es["query"])
 
-    _str_where = unwrap_query(est.query)
-
-    _query = f"""\
-{_str_arrow_urls}
-{_str_columns}
-filter_expression {{
-{indent(_str_where.to_protobuff(), INDENT)}
-}}
-
-max_rows: {MAX_ROWS}
-"""
-    print(_query)
+    _query = expr.to_output_query(columns=fields, arrow_urls=ARROW_PARTITIONS)
     return _query
-
-
 
 
 def unwrap_query(query) -> Expression:
     if "bool" not in query or len(query) > 1:
         invalid_keys = ", ".join(q for q in query if q != "bool")
-        raise NotImplemented(f"Can't support invalid keys: {invalid_keys}")
+        raise NotImplementedError(f"Can't support invalid keys: {invalid_keys}")
 
     return handle_generic_query(query["bool"])
 
@@ -133,35 +158,44 @@ def b(el: str):
     return f"({el})"
 
 
+def split_into_calls_of_two(function_name: str, iterable: List[Expression]):
+    if len(iterable) == 0:
+        return None
+    if len(iterable) == 1:
+        return iterable[0]
+
+    call = Call(function_name=function_name, arguments=iterable[-2:])
+    for i in range(len(iterable) - 2, 0, -1):
+        call = Call(function_name=function_name, arguments=[iterable[i], call])
+
+    return call
+
+
 def handle_generic_query(query):
     if isinstance(query, list):
         raise ValueError(f"Can't handle list {query}")
 
     if isinstance(query, (str, int, float, bool)):
-        raise ValueError(f"Expected dictionary, received: {query}")
+        return Literal(literal=query, literal_type=None)
 
     if "bool" in query:
         return handle_generic_query(query["bool"])
 
-    terms = []
-
-    keys = ", ".join(query.keys())
-
+    terms: List[Expression] = []
+    if "field" in query:
+        terms.append(Column(query["field"]))
     if "should" in query:
         should = query["should"]
-        inner = map(handle_generic_query, should)
-        if len(should) > 1:
-            terms.append(" OR ".join(map(b, inner)))
-        else:
-            terms.append(next(inner))
+        inner = list(map(handle_generic_query, should))
+        unwrapped_should = split_into_calls_of_two("or", inner)
+        if unwrapped_should:
+            terms.append(unwrapped_should)
     if "must_not" in query:
         must_not = query["must_not"]
-        inner = map(handle_generic_query, must_not)
-        if len(must_not) > 1:
-            inner_str = " AND ".join(inner)
-        else:
-            inner_str = next(inner)
-        terms.append(f"NOT {inner_str}")
+        unwrapped_must_not = split_into_calls_of_two(
+            "and", list(map(handle_generic_query, must_not))
+        )
+        terms.append(Call("invert", arguments=[unwrapped_must_not]))
     if "filter" in query:
         filters = query["filter"]
         terms.extend(map(handle_generic_query, filters))
@@ -193,32 +227,37 @@ def handle_generic_query(query):
     if len(terms) == 1:
         return terms[0]
     if len(terms) > 1:
-        return " AND ".join(map(b, terms))
+        return split_into_calls_of_two("and", terms)
 
     # raise NotImplementedError(f'Unhandled keys {keys}')
 
 
-def handle_range(query):
+def handle_range(query) -> Expression:
     statements = []
     for field, inner in query.items():
         op_map = {
-            "lt": "<",
-            "lte": "<=",
-            "gt": ">",
-            "gte": ">=",
+            "lt": "less",
+            "lte": "less_equal",
+            "gt": "greater",
+            "gte": "greater_equal",
         }
+        f = Column(name=field)
         for op in op_map:
             if op in inner:
-                statements.append(f"{field} {op_map[op]} {inner[op]}")
+                statements.append(
+                    Call(
+                        function_name=op_map[op],
+                        arguments=[f, handle_generic_query(inner[op])],
+                    )
+                )
+                # statements.append(f"{field} {op_map[op]} {inner[op]}")
 
-    if len(statements) > 1:
-        return " AND ".join(map(b, statements))
-    return statements[0]
+    return split_into_calls_of_two("and", statements)
 
 
-def handle_exists(exists):
-    field = exists["field"]
-    return f"{field} IS NOT NULL"
+def handle_exists(exists) -> Expression:
+    field = handle_generic_query(exists)
+    return Call("is_valid", arguments=[field])
 
 
 def quote_value(value):
@@ -230,25 +269,23 @@ def quote_value(value):
 
 def handle_terms(field, value):
     if isinstance(value, list):
-        prepped_values = ", ".join(map(quote_value, value))
-        return f"{field} in ({prepped_values})"
 
-    return f"{field}={quote_value(value)}"
+        prepped_values = ", ".join(map(quote_value, value))
+        raise NotImplementedError(f"{field} in ({prepped_values})")
+
+    return Call(
+        "string_list_contains_any",
+        arguments=[Column(name=field)],
+        options={"set_lookup_options": {"values": value}},
+    )
+    # return f"{field}={quote_value(value)}"
 
 
 def handle_match(field, value):
-    return f"{field} like '%{value}%'"
-
-
-class ProtobuffStrTests(TestCase):
-    def test_example_query(self):
-        expression = Call(
-            "less",
-            arguments=[Column("gnomad_exomes_AF"), Literal(0.0001, "float_value")],
-        )
-        val = expression.to_protobuff()
-        print(val)
-
-
-if __name__ == "__main__":
-    ProtobuffStrTests().test_example_query()
+    # return f"{field} like '%{value}%'"
+    # might be something simpler here
+    return Call(
+        "match_substring_regex",
+        arguments=[handle_generic_query(field)],
+        options=[f"pattern = .+{value}.+"],
+    )
